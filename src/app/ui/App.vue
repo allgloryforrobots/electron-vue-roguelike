5af98de935c753c929317200627b2f13d4102bc6
Илья Булатов <ilia.bulatov@infoguild.ru>
2025-08-11 14:56:22 +0300

    feature(player movement): add basic template

--- src/app/ui/App.vue
+++ src/app/ui/App.vue
@@ -7,13 +7,66 @@
 import { FOVCalculator, FOVGrid } from '@/widgets/FOVGrid';
 import { Grid } from '@/widgets/Grid'
 import { MapGenerator } from '@/widgets/Grid';
-import { ref } from 'vue';
+import { onMounted, onUnmounted, ref } from 'vue';
   const generator = new MapGenerator({ width: 50, height: 30, treeDensity: 0.05, clusterDensity: 0.008 });
   const generatedMap = generator.generateMap();
   const map = ref();
+  const fov = ref();
+  const fovMap = ref();
+
+  const playerX = ref(0);
+  const playerY = ref(0);
+
   map.value = generatedMap;
-  const fov = new FOVCalculator(map.value);
-  const fovMap = fov.calculateFOV(0, 0, 12);
+  fov.value = new FOVCalculator(map.value);
+  fovMap.value = fov.value.calculateFOV(playerX.value, playerY.value, 12);
+
+  // Функция проверки проходимости клетки
+  const isPassable = (x: number, y: number): boolean => {
+    if (!map.value) return false;
+    // Предполагаем, что 0 - проходимая клетка, 1 - непроходимая (стена/препятствие)
+    // Настройте это условие в соответствии с вашей системой обозначений
+    return map.value[y] !== undefined && map.value[y][x] !== undefined //&& map.value[y][x] === 0;
+  };
+
+  const handleKeyDown = (event: KeyboardEvent) => {
+    let newX = playerX.value;
+    let newY = playerY.value;
+    
+    switch(event.key) {
+      case 'ArrowUp':
+        newY -= 1;
+        break;
+      case 'ArrowDown':
+        newY += 1;
+        break;
+      case 'ArrowLeft':
+        newX -= 1;
+        break;
+      case 'ArrowRight':
+        newX += 1;
+        break;
+      default:
+        return; // Игнорируем другие клавиши
+    }
+
+    console.log(1)
+    // Проверяем, что новые координаты в пределах карты и клетка проходима
+    if (isPassable(newX, newY)) {
+    console.log(2)
+      playerX.value = newX;
+      playerY.value = newY;
+      fovMap.value = fov.value?.calculateFOV(playerX.value, playerY.value, 12);
+    }
+};
+
+onMounted(() => {
+  window.addEventListener('keydown', handleKeyDown);
+});
+
+onUnmounted(() => {
+  window.removeEventListener('keydown', handleKeyDown);
+});
 
 </script>
 <style>
