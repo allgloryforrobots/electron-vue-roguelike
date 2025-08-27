42aca348901d24176856ece7f7944de26ba0022c
Илья Булатов <ilia.bulatov@infoguild.ru>
2025-08-27 16:53:13 +0300

    refactoring(DDHeader): change css

--- src/shared/ui/DDHeader/DDHeader.vue
+++ src/shared/ui/DDHeader/DDHeader.vue
@@ -22,17 +22,14 @@
 <style scoped lang="scss">
     .page-header {
         text-align: center;
-        margin-bottom: 40px;
-        padding-bottom: 20px;
         border-bottom: 1px solid var(--border-color);
 
         &__title {
-            font-size: 2.8rem;
+            font-size: 24px;
             color: var(--accent-color-2);
             text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
             letter-spacing: 2px;
             font-variant: small-caps;
-            margin-bottom: 10px;
         }
 
         &__subtitle {
