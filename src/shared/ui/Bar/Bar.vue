1ca9ea6c04df0b49da8a885609eda1fb1f58a69a
Илья Булатов <ilia.bulatov@infoguild.ru>
2025-08-27 20:08:36 +0300

    refactoring(ui): minor changes

--- src/shared/ui/Bar/Bar.vue
+++ src/shared/ui/Bar/Bar.vue
@@ -8,14 +8,14 @@ const props = defineProps({
   },
   percent: {
     type: Number,
-    default: 100, // Уровень заполнения по умолчанию (в процентах)
+    default: 100,
     validator: (value: number) => value >= 0 && value <= 100,
   },
 })
 
 const barClass = computed(() => [
-  "bar",
-  props.variant,
+  "progress-bar__fill",
+  `progress-bar__fill--${props.variant}`,
 ])
 
 const barStyle = computed(() => ({
@@ -25,24 +25,24 @@ const barStyle = computed(() => ({
 </script>
 
 <template>
-  <div class="bar-container">
+  <div class="progress-bar">
     <div :class="barClass" :style="barStyle"></div>
-    <div class="dividers">
-      <div class="divider" style="left: 10%;"></div>
-      <div class="divider" style="left: 20%;"></div>
-      <div class="divider" style="left: 30%;"></div>
-      <div class="divider" style="left: 40%;"></div>
-      <div class="divider" style="left: 50%;"></div>
-      <div class="divider" style="left: 60%;"></div>
-      <div class="divider" style="left: 70%;"></div>
-      <div class="divider" style="left: 80%;"></div>
-      <div class="divider" style="left: 90%;"></div>
+    <div class="progress-bar__dividers">
+      <div class="progress-bar__divider" style="left: 10%;"></div>
+      <div class="progress-bar__divider" style="left: 20%;"></div>
+      <div class="progress-bar__divider" style="left: 30%;"></div>
+      <div class="progress-bar__divider" style="left: 40%;"></div>
+      <div class="progress-bar__divider" style="left: 50%;"></div>
+      <div class="progress-bar__divider" style="left: 60%;"></div>
+      <div class="progress-bar__divider" style="left: 70%;"></div>
+      <div class="progress-bar__divider" style="left: 80%;"></div>
+      <div class="progress-bar__divider" style="left: 90%;"></div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.bar-container {
+.progress-bar {
   position: relative;
   width: 400px;
   height: 5px;
@@ -50,34 +50,34 @@ const barStyle = computed(() => ({
   border-radius: 2px;
   overflow: hidden;
   margin: 20px 10px;
-}
 
-.bar {
-  height: 100%;
-  position: relative;
-  transition: width 0.3s ease;
-  filter: blur(2px);
-  background: var(--accent-color-3);
+  &__fill {
+    height: 100%;
+    position: relative;
+    transition: width 0.3s ease;
+    filter: blur(2px);
+    background: var(--accent-color-3);
 
-  &.mana {
-    background: var(--accent-color-6);
+    &--mana {
+      background: var(--accent-color-6);
+    }
   }
-}
 
-.divider {
-  position: absolute;
-  top: 0;
-  bottom: 0;
-  width: 1px;
-  background: var(--border-color);
-}
+  &__divider {
+    position: absolute;
+    top: 0;
+    bottom: 0;
+    width: 1px;
+    background: var(--border-color);
+  }
 
-.dividers {
-  position: absolute;
-  top: 0;
-  left: 0;
-  right: 0;
-  height: 100%;
-  pointer-events: none;
+  &__dividers {
+    position: absolute;
+    top: 0;
+    left: 0;
+    right: 0;
+    height: 100%;
+    pointer-events: none;
+  }
 }
 </style>
