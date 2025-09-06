3458570d8c8081d1afd868105c42aaac60d41a8c
Илья Булатов <ilia.bulatov@infoguild.ru>
2025-09-06 08:27:59 +0300

    feature(storybook): add storybook

---
+++ .storybook/main.ts
@@ -0,0 +1,26 @@
+import type { StorybookConfig } from '@storybook/vue3-vite';
+
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    "@chromatic-com/storybook",
+    "@storybook/addon-docs",
+    "@storybook/addon-onboarding",
+    "@storybook/addon-a11y",
+    "@storybook/addon-vitest"
+  ],
+  "framework": {
+    "name": "@storybook/vue3-vite",
+    "options": {}
+  },
+  async viteFinal(config) {
+    const { default: vue } = await import('@vitejs/plugin-vue');
+    config.plugins = config.plugins || [];
+    config.plugins.push(vue());
+    return config;
+  }
+};
+export default config;
--- eslint.config.js
+++ eslint.config.js
@@ -1,3 +1,6 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from "eslint-plugin-storybook";
+
 // eslint.config.js
 import js from '@eslint/js';
 import globals from 'globals';
@@ -5,70 +8,59 @@ import ts from 'typescript-eslint';
 import pluginVue from 'eslint-plugin-vue';
 import vueParser from 'vue-eslint-parser';
 
-export default [
-  ...pluginVue.configs['flat/recommended'],
-  // Базовые настройки
-  {
-    ignores: [
-      'node_modules/',
-      'dist/',
-      'build/',
-      '**/build',
-      'out/',
-      'coverage/',
-      '*.min.js'
-    ]
-  },
-
-  // JavaScript конфиг
-  js.configs.recommended,
-
-  // TypeScript конфиг
-  ...ts.configs.recommended,
-
-  // Vue конфиг
-  {
-    files: ['**/*.vue'],
-    languageOptions: {
-      parser: vueParser,
-      parserOptions: {
-        parser: ts.parser,
-        ecmaVersion: 'latest',
-        sourceType: 'module',
-        extraFileExtensions: ['.vue']
-      },
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
-    },
-    plugins: {
-      pluginVue
-    },
-    rules: {
-      'vue/multi-word-component-names': 'off',
-      'vue/html-self-closing': 'off',
-      'vue/max-attributes-per-line': 'off'
-    }
-  },
-
-  // Для JS/TS файлов
-  {
-    files: ['**/*.{js,ts}'],
-    languageOptions: {
+export default [...pluginVue.configs['flat/recommended'], // Базовые настройки
+{
+  ignores: [
+    'node_modules/',
+    'dist/',
+    'build/',
+    '**/build',
+    'out/',
+    'coverage/',
+    '*.min.js'
+  ]
+}, // JavaScript конфиг
+js.configs.recommended, // TypeScript конфиг
+...ts.configs.recommended, // Vue конфиг
+{
+  files: ['**/*.vue'],
+  languageOptions: {
+    parser: vueParser,
+    parserOptions: {
       parser: ts.parser,
-      parserOptions: {
-        ecmaVersion: 'latest',
-        sourceType: 'module'
-      },
-      globals: {
-        ...globals.browser,
-        ...globals.node
-      }
+      ecmaVersion: 'latest',
+      sourceType: 'module',
+      extraFileExtensions: ['.vue']
     },
-    rules: {
-      'no-console': 'warn',
-      'no-debugger': 'error'
+    globals: {
+      ...globals.browser,
+      ...globals.node
     }
+  },
+  plugins: {
+    pluginVue
+  },
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/html-self-closing': 'off',
+    'vue/max-attributes-per-line': 'off'
   }
-];
+}, // Для JS/TS файлов
+{
+  files: ['**/*.{js,ts}'],
+  languageOptions: {
+    parser: ts.parser,
+    parserOptions: {
+      ecmaVersion: 'latest',
+      sourceType: 'module'
+    },
+    globals: {
+      ...globals.browser,
+      ...globals.node
+    }
+  },
+  rules: {
+    'no-console': 'warn',
+    'no-debugger': 'error'
+  }
+}, ...storybook.configs["flat/recommended"]];
