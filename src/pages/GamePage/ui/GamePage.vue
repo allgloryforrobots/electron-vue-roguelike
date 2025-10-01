a7b33e65b7137c2aaf6f59f42e32b4f353f98726
Илья Булатов <ilia.bulatov@infoguild.ru>
2025-10-01 09:36:08 +0300

    refactoring(grids): global map-gris styles

--- src/pages/GamePage/ui/GamePage.vue
+++ src/pages/GamePage/ui/GamePage.vue
@@ -102,5 +102,16 @@
   <FOVGrid v-if="map" :map="map" />
 </template>
 
-<style scoped lang="scss">
+<style>
+.map-grid {
+  display: grid;
+  grid-template-columns: repeat(var(--grid-width), var(--cell-size));
+  grid-template-rows: repeat(var(--grid-height), var(--cell-size));
+  position: fixed;
+  top: var(--navbar-height);
+  left: 0;
+  width: calc(var(--grid-width) * var(--cell-size));
+  height: calc(var(--grid-height) * var(--cell-size));
+}
+
 </style>
